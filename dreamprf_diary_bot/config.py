f37f73cbef690faa062f6468dd2d11d5b9747f75
Nikolai Riabykh <nryabykh@isgneuro.com>
2023-02-03 17:46:04 +0600

    move spreadsheer_id and constants here

--- dreamprf_diary_bot/config.py
+++ dreamprf_diary_bot/config.py
@@ -1,17 +1,27 @@
+import os
 from pathlib import Path
 
 import tomli
+
+TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_DREAMPRF_TOKEN')
+RANGE = '0-14 дней!A:DH'
+DATES_RANGE = '0-14 дней!A3:GH3'
+
+TIME_COL = 3
+NOTES_COL = 4
+PERIOD = 8
+DAY_ROW_INDEX = 6
+DAY_LENGTH = 6
+EVENING_ROW_INDEX = 38
+EVENING_LENGTH = 6
+NIGHT_ROW_INDEX = 46
+NIGHT_LENGTH = 12
 
 
 def get_cred_file():
     folder = _get_secrets_folder()
     config = _get_config()
     return folder / config['google']['credentials_filename']
-
-
-def get_spreadsheet_id():
-    config = _get_config()
-    return config['google']['spreadsheet_id']
 
 
 def _get_config():
@@ -23,3 +33,11 @@ def _get_config():
 
 def _get_secrets_folder():
     return Path(__file__).parent.parent / '.secrets'
+
+
+def get_spreadsheet_id():
+    config = _get_config()
+    return config['google']['spreadsheet_id']
+
+
+SPREADSHEET_ID = get_spreadsheet_id()
