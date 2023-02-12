2d59bdaa04ca31fbe16cac86ed015d2ce3a91745
Nikolai Riabykh <nryabykh@isgneuro.com>
2023-02-13 00:57:30 +0600

    (refactoring) clear main module, move stuff to config and static

---
+++ dreamprf_diary_bot/__main__.py
@@ -0,0 +1,77 @@
+import asyncio
+import logging
+
+from aiogram import Bot, Dispatcher
+from aiogram.contrib.fsm_storage.files import JSONStorage
+from aiogram.dispatcher.filters import Text
+from aiogram.types import BotCommand
+
+from dreamprf_diary_bot import config, handlers
+from dreamprf_diary_bot.services.night_register import NightRegister
+
+logging.basicConfig(
+    format="%(asctime)s - %(levelname)s - %(name)s - %(message)s", level=logging.INFO
+)
+logger = logging.getLogger(__name__)
+
+if not config.TELEGRAM_BOT_TOKEN:
+    raise ValueError("TELEGRAM_BOT_TOKEN env variable should be initialized in .env.")
+
+
+async def set_commands(bot: Bot):
+    commands = [
+        BotCommand(command="/start", description="Привет!"),
+        BotCommand(command="/help", description="Справка по работе"),
+        BotCommand(command="/night", description="Включить режим записи НП"),
+        BotCommand(command="/last", description="Посмотреть и отредактировать НП последней ночи"),
+        BotCommand(command="/end", description="Выйти из режима записи НП"),
+    ]
+    await bot.set_my_commands(commands)
+
+
+async def main():
+    bot = Bot(token=config.TELEGRAM_BOT_TOKEN)
+    dp = Dispatcher(bot, storage=JSONStorage(config.STATE_PATH))
+
+    command_handlers = {
+        'start': handlers.start,
+        'help': handlers.help_,
+        'night': handlers.night,
+        'end': handlers.end,
+        'last': handlers.last,
+    }
+
+    text_handlers = [
+        {'handler': handlers.add_wake, 'filters': [Text('Добавить НП')], 'state': NightRegister.start},
+        {'handler': handlers.add_note, 'filters': [], 'state': NightRegister.start},
+        {'handler': handlers.register, 'filters': [Text(startswith='https://docs.google.com')], 'state': '*'}
+    ]
+
+    callback_handlers = [
+        {
+         'handler': handlers.select_time_to_edit,
+         'filters': [Text(startswith='edit_note_')],
+         'state': NightRegister.start
+        }
+    ]
+
+    for cmd_name, cmd_handler in command_handlers.items():
+        dp.register_message_handler(cmd_handler, commands=cmd_name, state='*')
+
+    for handler in text_handlers:
+        dp.register_message_handler(handler['handler'], *handler['filters'], state=handler['state'])
+
+    for handler in callback_handlers:
+        dp.register_callback_query_handler(handler['handler'], *handler['filters'], state=handler['state'])
+
+    await set_commands(bot)
+
+    await dp.start_polling()
+
+
+if __name__ == '__main__':
+    try:
+        asyncio.run(main())
+    except Exception:
+        import traceback
+        logger.warning(traceback.format_exc())
--- dreamprf_diary_bot/config.py
+++ dreamprf_diary_bot/config.py
@@ -1,46 +1,62 @@
-import json
 import os
 from pathlib import Path
-from typing import Optional
 
-import tomli
+from dotenv import load_dotenv
 
+load_dotenv()
+
+# environment variables
 TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_DREAMPRF_TOKEN')
+CRED_FILENAME = os.getenv('DREAMPRF_GSHEET_CRED_PATH')
+
+# internal file paths
+BASE_DIR = Path(__file__).resolve().parent
+STATE_PATH = './db/state.json'
+USERS_PATH = './db/user_sid.json'
+
+# default sheet names
+FIRST_SHEET = '0-14 дней'
+SECOND_SHEET = '14-27 дней'
+
+# default sheet ranges
 RANGE = 'A:DH'
 DATES_RANGE = 'A3:GH3'
 
+# default ranges and column and row numbers
 TIME_COL = 3
 NOTES_COL = 4
-PERIOD = 8
 DAY_ROW_INDEX = 6
-DAY_LENGTH = 6
 EVENING_ROW_INDEX = 38
 EVENING_LENGTH = 6
 NIGHT_ROW_INDEX = 46
 NIGHT_LENGTH = 12
+PERIOD = 8
+DAY_LENGTH = 6
+
+DATE_FORMAT = '%d.%m.%Y'
+TIME_FORMAT = '%H:%M'
+
+# def get_cred_file():
+#     folder = _get_secrets_folder()
+#     config = _get_config()
+#     return folder / config['google']['credentials_filename']
 
 
-def get_cred_file():
-    folder = _get_secrets_folder()
-    config = _get_config()
-    return folder / config['google']['credentials_filename']
+# def _get_config():
+#     folder = _get_secrets_folder()
+#     with open(folder / 'secrets.toml', 'rb') as f:
+#         config = tomli.load(f)
+#     return config
 
 
-def _get_config():
-    folder = _get_secrets_folder()
-    with open(folder / 'secrets.toml', 'rb') as f:
-        config = tomli.load(f)
-    return config
+# def _get_secrets_folder():
+#     return Path(__file__).parent.parent / '.secrets'
 
 
-def _get_secrets_folder():
-    return Path(__file__).parent.parent / '.secrets'
-
-
-def get_spreadsheet_id(user_id: int) -> Optional[str]:
-    if not os.path.exists('user_sid.json'):
-        return None
-
-    with open('user_sid.json', 'r') as f:
-        data = json.load(f)
-    return data.get(str(user_id), None)
+# def get_spreadsheet_id(user_id: int) -> Optional[str]:
+#     if not os.path.exists('user_sid.json'):
+#         return None
+#
+#     with open('user_sid.json', 'r') as f:
+#         data = json.load(f)
+#     return data.get(str(user_id), None)
